00b19490f4bb4e9a3998975e3249506723a3f550
Yen Ling Tan <yt2dh@virginia.edu>
2026-01-10 13:23:14 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Stellar by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	var	$window = $(window),
@@ -119,5 +113,6 @@
 		$('.scrolly').scrolly({
 			speed: 1000
 		});
+
 
 })(jQuery);
